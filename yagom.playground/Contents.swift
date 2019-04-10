95a49b7b729c452c63d610476a29edf4e1a8f808
kiljh <kiljh@me.com>
2019-04-10 18:02:44 +0900

    Step7

--- yagom.playground/Contents.swift
+++ yagom.playground/Contents.swift
@@ -240,3 +240,90 @@ let sortedUnion: [Int] = union.sorted()
 let intersection: Set<Int> = setA.intersection(setB)
 
 let subtracting: Set<Int> = setA.subtracting(setB)
+
+
+// 7강 함수 기본
+
+//MARK: - 함수의 선언
+
+//MARK: 함수선언의 기본형태
+//func 함수이름(매개변수1이름: 매개변수1타입, 매개변수2이름: 매개변수2타입 ...) -> 반환타입 {
+//    함수 구현부
+//    return 반환값
+//}
+
+func sum(a: Int, b: Int) -> Int {
+    return a + b
+}
+
+
+
+//MARK: 반환 값이 없는 함수
+//func 함수이름(매개변수1이름: 매개변수1타입, 매개변수2이름: 매개변수2타입 ...) -> Void {
+//    함수 구현부
+//    return
+//}
+
+func printMyName(name: String) -> Void {
+    print(name)
+}
+
+
+
+
+//func 함수이름(매개변수1이름: 매개변수1타입, 매개변수2이름: 매개변수2타입 ...) {
+//    함수 구현부
+//    return
+//}
+
+func printYourName(name: String) {
+    print(name)
+}
+
+
+
+
+//MARK: 매개변수가 없는 함수
+//func 함수이름() -> 반환타입 {
+//    함수 구현부
+//    return 반환값
+//}
+
+func maximumIntegerValue() -> Int {
+    return Int.max
+}
+
+
+
+//MARK: 매개변수와 반환값이 없는 함수
+//func 함수이름() -> Void {
+//    함수 구현부
+//    return
+//}
+
+func hello() -> Void { print("hello") }
+
+
+
+//func 함수이름() {
+//    함수 구현부
+//    return
+//}
+
+func bye() { print("bye") }
+
+
+
+//MARK: - 함수의 호출
+
+sum(a: 3, b: 5) // 8
+
+printMyName(name: "yagom") // yagom
+
+printYourName(name: "hana") // yagom
+
+maximumIntegerValue() // Int의 최댓값
+
+hello() // heloo
+
+bye() // bye
