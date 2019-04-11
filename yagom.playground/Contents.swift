4b6e48f99155307bf976d3c14634daf7c6e5614e
kiljh <kiljh@me.com>
2019-04-11 21:31:35 +0900

    Step14

--- yagom.playground/Contents.swift
+++ yagom.playground/Contents.swift
@@ -662,10 +662,92 @@ var yagom13: Student = Student()
 yagom13.name = "yagom"
 yagom13.class = "스위프트"
 
-let jina: Student = Student()
+let jina13: Student = Student()
 
 // 불변 인스턴스이므로 프로퍼티 값 변경 불가
 // 컴파일 오류 발생
 //jina.name = "jina"
-jina.selfIntroduce() // 저는 Swift반 unknown입니다
+jina13.selfIntroduce() // 저는 Swift반 unknown입니다
 
+
+
+// 14강 클래스
+
+//MARK: - 정의
+
+class 이름 {
+    /* 구현부 */
+}
+
+//MARK: 프로퍼티 및 메서드
+
+class Sample14 {
+    var mutableProperty: Int = 100 // 가변 프로퍼티
+    let immutableProperty: Int = 100 // 불변 프로퍼티
+    
+    static var typeProperty: Int = 100 // 타입 프로퍼티
+    
+    // 인스턴스 메서드
+    func instanceMethod() {
+        print("instance method")
+    }
+    
+    // 타입 메서드
+    // 재정의 불가 타입 메서드 - static
+    static func typeMethod() {
+        print("type method - static")
+    }
+    
+    // 재정의 가능 타입 메서드 - class
+    class func classMethod() {
+        print("type method - class")
+    }
+}
+
+//MARK: 클래스 사용
+
+var mutableReference: Sample14 = Sample14()
+
+mutableReference.mutableProperty = 200
+//mutableReference.immutableProperty = 200
+
+let immutableReference: Sample14 = Sample14()
+
+immutableReference.mutableProperty = 200
+//immutableReference.immutableProperty = 200
+
+//immutableReference = mutableReference
+
+
+// 타입 프로퍼티 및 메서드
+Sample14.typeProperty = 300
+Sample14.typeMethod() // type method
+
+//mutableReference.typeProperty = 400
+
+
+//MARK - 학생 클래스
+
+class Student14 {
+    var name: String = "unknown"
+    var `class`: String = "Swift"
+    
+    class func selfIntroduce() {
+        print("학생타입입니다")
+    }
+    
+    func selfIntroduce() {
+        print("저는 \(self.class)반 \(name)입니다")
+    }
+}
+
+Student14.selfIntroduce() // 학생타입입니다
+
+var yagom14: Student14 = Student14()
+yagom14.name = "yagom"
+yagom14.class = "스위프트"
+yagom14.selfIntroduce()     // 저는 스위프트반 yagom입니다
+
+let jina14: Student14 = Student14()
+jina14.name = "jina"
+jina14.selfIntroduce()    // 저는 Swift반 jina입니다
