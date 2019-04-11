a002593278bd268dd9a14f06c4331ee57952a468
kiljh <kiljh@me.com>
2019-04-11 23:11:33 +0900

    Step16

--- yagom.playground/Contents.swift
+++ yagom.playground/Contents.swift
@@ -862,3 +862,32 @@ enum Month {
 }
 
 Month.mar.printMessage()
+
+
+
+// 16강 값 타입과 참조 타입
+
+//MARK:- Class vs Struct/Enum
+
+struct ValueType {
+    var property = 1
+}
+
+class ReferenceType {
+    var property = 1
+}
+
+let firstStructInstance = ValueType()
+var secondStructInstance = firstStructInstance
+secondStructInstance.property = 2
+
+print("first struct instance property : \(firstStructInstance.property)")       // 1
+print("second struct instance property : \(secondStructInstance.property)")     // 2
+
+
+let firstClassReference = ReferenceType()
+var secondClassReference = firstClassReference
+secondClassReference.property = 2
+
+print("first class reference property : \(firstClassReference.property)")       // 2
+print("second class reference property : \(secondClassReference.property)")     // 2
